dded19e97b1dcb9c28da2346e89d42dbd1852dbd
areeba-waheed <areeba.waheed.97@gmail.com>
2018-10-07 02:36:20 -0700

    replace All words in a file

---
+++ src/FileSearch.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+public class FileSearch extends SwingWorker<Integer, String> {
+    private File directory;
+    private String word;
+    private JTextArea area;
+
+    private static void failIfInterrupted() throws InterruptedException {
+        if (Thread.currentThread().isInterrupted()) {
+            throw new InterruptedException("Interrupted while searching files");
+        }
+    }
+
+    public FileSearch(File directory, String word, JTextArea area) {
+        this.directory = directory;
+        this.word = word;
+        this.area = area;
+    }
+
+    @Override
+    protected Integer doInBackground() throws Exception {
+        int matches =0;
+        publish("Listing all files under the directory " + directory);
+
+        final List<File> files = new ArrayList<>(FileUtils.listFiles(directory, new SuffixFileFilter(".txt"), TrueFileFilter.TRUE));
+        for(int i =0; i<files.size(); i++) {
+            FileSearch.failIfInterrupted();
+            final File file = files.get(i);
+            publish ("Seatching file: "+ file);
+            String text = FileUtils.readFileToString(file);
+            matches += StringUtils.countMatches(text, word);
+
+        }
+        return matches;
+    }
+
+    @Override
+    protected void process(final List<String> chunks) {
+        // Updates the messages text area
+        for (final String string : chunks) {
+            area.append(string);
+            area.append("\n");
+        }
+    }
+
+
+
+}
--- src/FindReplace.java
+++ src/FindReplace.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,6 +10,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.prefs.Preferences;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FindReplace extends JFrame {
 
@@ -16,6 +23,7 @@ public class FindReplace extends JFrame {
     private ActionListener actionListener;
 
     private JFileChooser chooser;
+    private File selectedFile;
 
     //textfields
     private JTextField findStringTab1;
@@ -67,6 +75,8 @@ public class FindReplace extends JFrame {
     //arrays
     private ArrayList<String> wordList;
 
+    private FileSearch fileSearch;
+
 
 
     public FindReplace(){
@@ -98,18 +108,91 @@ public class FindReplace extends JFrame {
         public void actionPerformed(ActionEvent e) {
             if(e.getActionCommand() == "Search") {
                 openFile();
+                try {
+                    findWords(selectedFile, false, findStringTab1.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
                 printTextArea(textAreaTab1);
-            } else if(e.getActionCommand() == "Find") {
+            }
+            else if(e.getActionCommand() == "Find") {
                 openFile();
+                try {
+                    findWords(selectedFile, false, findStringTab2.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
                 printTextArea(textAreaTab2);
             } else if(e.getActionCommand() == "Replace") {
-                replaceWords();
-            }
-        }
+                int res = popUp();
+                if(res == JOptionPane.OK_OPTION) {
+                    try {
+                        findWords(selectedFile, true, findStringTab2.getText());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    try {
+                        replaceWords();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    textAreaTab2.setText("File updated");
+                }
+            } else if(e.getActionCommand() == "Browse") {
+                openDirectory();
+                textAreaTab3.setText("Searching for word '" + findStringTab3.getText() + "' in text files under: " + selectedFile.getAbsolutePath() + "\n");
+                fileSearch = new FileSearch(selectedFile, findStringTab3.getText(), textAreaTab3);
+
+        }      }
     }
 
-    private void replaceWords(){
-        
+    private void openDirectory() {
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        int result = chooser.showOpenDialog(this);
+        if(result == JFileChooser.APPROVE_OPTION) {
+            selectedFile = chooser.getCurrentDirectory();
+        }
+
+
+    }
+    private int popUp() {
+        int result = JOptionPane.showConfirmDialog(null, "Are you sure ?", "Attention", JOptionPane.OK_CANCEL_OPTION);
+        return result;
+    }
+    private void printUpdatedFile(JTextArea area) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(selectedFile));
+        String line = null;
+        wordList = new ArrayList<>();
+        while((line = br.readLine()) != null) {
+            wordList.add("Line number: " + line.indexOf(line) + " "+line+"\n");
+        }
+
+    }
+
+    private void replaceWords() throws IOException {
+        //Writer writer = new BufferedWriter();
+
+        File fileToBeModified = selectedFile;
+        String oldContent="";
+        BufferedReader reader = new BufferedReader(new FileReader(fileToBeModified));
+        String line = reader.readLine();
+        while(line != null){
+            oldContent = oldContent+line+System.lineSeparator();
+            line = reader.readLine();
+        }
+        String newContent = oldContent.replaceAll(findStringTab2.getText(), replaceStringTab2.getText());
+
+        FileWriter writer = new FileWriter(fileToBeModified);
+        writer.write(newContent);
+        reader.close();
+        writer.close();
+
+
+
+
+
     }
 
     private void openFile() {
@@ -117,8 +200,8 @@ public class FindReplace extends JFrame {
         chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         int result = chooser.showOpenDialog(this);
         if(result == JFileChooser.APPROVE_OPTION) {
-             File selectedFile = chooser.getSelectedFile();
-             findWords(selectedFile);
+            selectedFile = chooser.getSelectedFile();
+             //findWords(selectedFile);
             //tab1TextArea.setText("Searching for "+ word +" in the text file named " +selectedFile.getAbsolutePath() );
         }
 
@@ -130,24 +213,44 @@ public class FindReplace extends JFrame {
         }
     }
 
-    private void findWords(File file) {
+    private void findWords(File file, Boolean flag, String word) throws IOException {
         //JTextArea textArea = area;
-        String word = findStringTab1.getText();
+        //String word = findStringTab1.getText();
         wordList = new ArrayList<>();
+        //FileReader fr = new FileReader(file);
         try {
             String s="";
             int count =0;
             BufferedReader bf = new BufferedReader(new FileReader(file));
             while( (s=bf.readLine() )!=null) {
                 count++;
-                if(s.contains(word)){
+                //if (wholeCaseTab1.isSelected()) {
+                    //word = word + " ";
+                    if (s.contains(word)) {
 
-                    //textArea.append("Line number: " +count+" "+ s +"\n");
-                    wordList.add("Line number: " + count + " "+s+"\n");
+                        wordList.add("Line number: " + count + " " + s + "\n");
+                  //  }
+                //}
+                //else {
+                    //if (s.contains(word)) {
+
+                        //wordList.add("Line number: " + count + " " + s + "\n");
+                  //  }
+                }
+               //textArea.append("Line number: " +count+" "+ s +"\n");
+
+                    /*if(flag == true) {
+                        s.replaceAll(word, replaceStringTab2.getText());
+
+                    }
+                    else{
+                        wordList.add("Line number: " + count + " "+s+"\n");
+                    }*/
+
                     //lineNumber.add(count);
                 }
 
-            }
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -158,14 +261,18 @@ public class FindReplace extends JFrame {
     }
 
     private void createTextFields(){
+        wordList = new ArrayList<>();
 
         findStringTab1 = new JTextField(25);
         findStringTab2 = new JTextField(25);
-        findStringTab3 = new JTextField(25);
+        findStringTab3 = new JTextField(23);
         replaceStringTab2 = new JTextField(25);
-        replaceStringTab3 = new JTextField(25);
-        filter = new JTextField(25);
+        replaceStringTab3 = new JTextField(22);
+        filter = new JTextField(15);
 
+       /* findStringTab1.getDocument().addDocumentListener(new MyListener());
+        Preferences prefs = Preferences.userRoot().node("value");
+        findStringTab1.setText(prefs.get("key", ""));*/
 
         findStringTab1.addActionListener(actionListener);
         findStringTab2.addActionListener(actionListener);
@@ -173,6 +280,30 @@ public class FindReplace extends JFrame {
         replaceStringTab2.addActionListener(actionListener);
         replaceStringTab3.addActionListener(actionListener);
         filter.addActionListener(actionListener);
+    }
+
+    class MyListener implements DocumentListener {
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            final Document document = e.getDocument();
+            Preferences prefs = Preferences.userRoot().node("value");
+            try{
+                prefs.put("key", document.getText(0, document.getLength()));
+            }catch(BadLocationException event) {
+                event.printStackTrace();
+            }
+        }
     }
 
     private void createButtons(){
@@ -314,7 +445,7 @@ public class FindReplace extends JFrame {
         findAllPanel.add(matchCaseTab3, BorderLayout.CENTER);
         findAllPanel.add(wholeCaseTab3, BorderLayout.CENTER);
 
-        findAllPanel.add(textAreaTab3, BorderLayout.PAGE_END);
+        findAllPanel.add(paneTab3, BorderLayout.PAGE_END);
 
     }
 }
